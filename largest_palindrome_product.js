257a051dc85db27bdd760823158b32140c015218
Andre Constantin <andrei@constantin.rocks>
2016-05-04 21:14:11 +0300

    created is_palindrome() as a function

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -2,25 +2,27 @@
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
-//module.exports.multiples_5_3_sum = multiples_5_3_sum;
+module.exports.largest_palindromic_up_to_n = largest_palindromic_up_to_n;
+module.exports.is_palindrome = is_palindrome;
+
+function is_palindrome(num){
+	var digits = num.toString();
+	var inverse = num.toString().split('').reverse().join('');
+
+	return digits === inverse;
+}
 
 function largest_palindromic_up_to_n (n){
     var product=0;
     var max_product = 0
     for (var i = n-1; i>99; i-=1){
         for (var j = n-1; j>i; j-=1){
-			product = i*j;
-			var digits = product.toString().split('').join('');
-	        var inverse = product.toString().split('').reverse().join('');
-			if (digits==inverse && product>max_product){
-				console.log(i,j, max_product, product);
+			product = i*j;			
+			if ( is_palindrome( product ) && product>max_product){
 				max_product = product;
-
-
 			}
 			
         }
-
 	}
   return max_product;  
 }
