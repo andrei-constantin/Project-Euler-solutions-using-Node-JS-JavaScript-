97ca880660c444e5b1a983bfa7668618329b40dc
Andre Constantin <andrei@constantin.rocks>
2016-05-09 20:22:51 +0300

    reuse the prime_number function as is_prime()

--- 10001_st_prime.js
+++ 10001_st_prime.js
@@ -1,28 +1,13 @@
 /*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10 001st prime number? */
-var primes = [2, 3, 5];
-function is_prime (n){
-		
-		for (var i = 0; i < primes.length; i++){
-			if ( n%primes[i] === 0)
-				return false;
-		};
 
-		var sqrt = Math.floor(Math.sqrt(n));
-		for (i = primes[ primes.length - 1 ] ;i<=sqrt;i += 2){
-			if( n%i === 0)
-				return false;
-		};
+var is_prime = require('./largest_prime_factor.js').is_prime;
+var primes = [2,3,5];
 
-		primes.push( n );
-		
-	};
-	
 for (var term = 7;primes.length<10002;term+=2){	
-	is_prime(term);	
-	}
-
+	is_prime(term, primes);	
+}
 
 console.log(primes[10000]);
 
--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -1,10 +1,9 @@
 /*The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
+module.exports.is_prime = prime_number;
 
-var primes = [2, 3, 5];
-
-function prime_number (n){
+function prime_number (n, primes){
 		
 		for (var i = 0; i < primes.length; i++){
 			if ( n%primes[i] === 0)
@@ -21,17 +20,19 @@ function prime_number (n){
 		return true;
 };
 		
+var primes = [2, 3, 5];
 function largest_prime_factor(m){
-	if ( prime_number( m ) ){
+	if ( prime_number( m, primes ) ){
 		return m;
 	}
 
 	for (n=2;n<=m;n++){
-		if (prime_number(n) && m%n === 0){
+		if (prime_number(n, primes) && m%n === 0){
 			return largest_prime_factor( m / n);
 		};
 	};
 
 };
 
-console.log(largest_prime_factor(600851475143));
+//console.log(largest_prime_factor(600851475143));
+
