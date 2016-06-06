5bb991dbe7b17e3313b71fa2326f332ff125dd1a
Andre Constantin <andrei@constantin.rocks>
2016-06-06 18:19:12 +0300

    Added tests for is_prime and largest_prime_factor

--- test/test_all.js
+++ test/test_all.js
@@ -9,11 +9,14 @@ var nthFibonacci = require('../even_fibonacci_numbers').nthFibonacci,
 
 var nth_prime = require('../10001_st_prime.js').nth_prime;
 
+var is_prime = require('../largest_prime_factor.js').is_prime;
+
+var largest_prime_factor = require('../largest_prime_factor.js').largest_prime_factor;
+
 var divisors = require('../highly_divisible_triangular_number.js').divisors;
 
 var large_sum = require('../large_sum.js').large_sum;
 
-var largest_palindromic_up_to_n = require('../largest_palindrome_product.js').largest_palindromic_up_to_n;
 var is_palindrome = require('../largest_palindrome_product.js').is_palindrome;
 
 
@@ -41,10 +44,26 @@ suite('Even fibo numbers sum', function() {
 
 suite('Nth prime', function() {
     test('5th prime number is 11', function() {
-        assert.equal(11, nth_prime(5));
-    });
-    test('6th prime number is 17', function() {
+        assert.equal(11, nth_prime(5)); }); test('6th prime number is 17', function() {
         assert.equal(13, nth_prime(6));
+    });
+});
+
+suite('Is prime', function() {
+    test('97 is a prime number', function() {
+        assert.equal(true, is_prime(11, [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187]));
+    });
+    test('93 is not a prime number', function() {
+        assert.equal(false, is_prime(93, [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187]));
+    });
+});
+
+ suite('Largest prime factor', function() {
+    test('Largest prime factor for 1700 is 17', function() {
+        assert.equal(17, largest_prime_factor(1700));
+    });
+    test('Largest prime factor for 5100 is 17', function() {
+        assert.equal(17, largest_prime_factor(5100));
     });
 });
 
